1d288b59eb856559ad565913455bcd04533f5fa7
Somiran Roy <somiran@gmail.com>
2023-08-07 19:08:56 -0700

    Modified files

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,7 +4,7 @@ const Navbar = () => {
             <h1>This is Somiran blog</h1>
             <div>
                 <a href="">Home</a>
-                <a href="/create" style={{
+                <a href="/create" style={{    // One brace to say, we have dynamic content and the other to import json data
                     color:'white',
                     backgroundColor:'#f1356d',
                     borderRadius: '8px'
